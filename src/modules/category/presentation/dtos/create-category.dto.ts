44a6c24371adee411995c1dad8857050766cb831
jpmagiero <jpaulomr12@gmail.com>
2025-04-16 20:27:30 -0300

    :sparkles: add swegger

--- src/modules/category/presentation/dtos/create-category.dto.ts
+++ src/modules/category/presentation/dtos/create-category.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
+  @ApiProperty({ example: 'Roupas', description: 'Nome da categoria' })
   @IsString()
   name: string;
 }
--- src/modules/item/presentation/dtos/create-item.dto.ts
+++ src/modules/item/presentation/dtos/create-item.dto.ts
@@ -1,27 +1,35 @@
 import { IsString, IsOptional, IsNumber, IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateItemDto {
+  @ApiProperty({ example: 'Jaqueta jeans', description: 'Nome do item' })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'Zara', description: 'Marca do item' })
   @IsOptional()
   @IsString()
   brand?: string;
 
+  @ApiProperty({ example: 'M', description: 'Tamanho do item' })
   @IsOptional()
   @IsString()
   size?: string;
 
+  @ApiProperty({ example: 'Preto', description: 'Cor do item' })
   @IsOptional()
   @IsString()
   color?: string;
 
+  @ApiProperty({ example: 'Novo', description: 'Status do item' })
   @IsString()
   status: string;
 
+  @ApiProperty({ example: 100, description: 'Preço do item' })
   @IsNumber()
   price: number;
 
+  @ApiProperty({ example: 1, description: 'ID da categoria' })
   @IsInt()
   categoryId: number;
 }
