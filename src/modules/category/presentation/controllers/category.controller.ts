fb269e8087c7c2083700e2fe7d38bc6d8dba9581
jpmagiero <jpaulomr12@gmail.com>
2025-04-16 21:26:28 -0300

    :sparkles: allow creating multiple items and categories in bulk

--- src/modules/category/presentation/controllers/category.controller.ts
+++ src/modules/category/presentation/controllers/category.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateCategoryUseCase } from '../../application/use-cases/create-category.use-case';
 import { CreateCategoryDto } from '../dtos/create-category.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly createCategoryUseCase: CreateCategoryUseCase) {}
 
   @Post()
-  async create(@Body() dto: CreateCategoryDto) {
-    return this.createCategoryUseCase.execute(dto);
+  @ApiBody({ type: [CreateCategoryDto] })
+  async create(@Body() dtos: CreateCategoryDto[]) {
+    return Promise.all(
+      dtos.map((dto) => this.createCategoryUseCase.execute(dto)),
+    );
   }
 }
--- src/modules/item/presentation/controllers/item.controller.ts
+++ src/modules/item/presentation/controllers/item.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateItemUseCase } from '../../application/use-cases/create-item.use-case';
 import { CreateItemDto } from '../dtos/create-item.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('items')
 export class ItemController {
   constructor(private readonly createItemUseCase: CreateItemUseCase) {}
 
   @Post()
-  async create(@Body() dto: CreateItemDto) {
-    return this.createItemUseCase.execute(dto);
+  @ApiBody({ type: [CreateItemDto] })
+  async create(@Body() dtos: CreateItemDto[]) {
+    return Promise.all(dtos.map((dto) => this.createItemUseCase.execute(dto)));
   }
 }
