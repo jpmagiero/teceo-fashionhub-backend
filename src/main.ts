94fd68c30b26c7aabcea36a204bb2ed338f80340
jpmagiero <jpaulomr12@gmail.com>
2025-04-20 19:33:14 -0300

    :sparkles: add enableCors

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   await app.listen(3000);
   console.log('Application is running on: http://localhost:3000');
 }
